085e71c1408684490442901bc0946fa75c16a2ee
mayor falomo <mayorfalomo@gmail.com>
2024-07-03 08:03:52 +0100

    added body parser

--- index.js
+++ index.js
@@ -4,12 +4,15 @@ import mongoose from "mongoose";
 import cors from "cors";
 import userRouter from "./route/userRoute.js";
 import paymentRouter from "./route/paymentsHandler.js";
+import bodyParser from "body-parser";
 const app = express();
 const port = 7000;
 dotEnv.config({ path: "./vars/.env" });
 
 app.use(express.json());
 app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose
   .connect(process.env.MONGO_URL, {
